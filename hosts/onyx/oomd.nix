42e5cd34c493e1a76a59bcaa4781ebb12caf0633
Vladyslav Afanasiev <kutabe@protonmail.com>
2025-08-01 05:40:53 +0300

    set memory pressure limit

--- hosts/onyx/oomd.nix
+++ hosts/onyx/oomd.nix
@@ -9,11 +9,13 @@
   
   systemd.slices."user-".sliceConfig = {
     ManagedOOMMemoryPressure = "kill";
+    ManagedOOMMemoryPressureLimit = "85%";
     ManagedOOMSwap = "kill";
   };
 
   systemd.slices."system-".sliceConfig = {
     ManagedOOMMemoryPressure = "kill";
+    ManagedOOMMemoryPressureLimit = "90%";
     ManagedOOMSwap = "kill";
   };
 }
