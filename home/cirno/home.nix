d8c33627a7f73708e9eca3470c262527316be4bd
Vladyslav Afanasiev <kutabe@protonmail.com>
2025-08-01 10:26:25 +0300

    configure sops, add declarative nextcloud with rclone & sops

--- home/cirno/home.nix
+++ home/cirno/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     inputs.zen-browser.homeModules.default
+    inputs.sops-nix.homeManagerModules.sops
     ./gaming.nix
   ];
 
@@ -38,7 +39,52 @@
     discord
     krita
     
+    kdePackages.plasma-vault
   ];
+
+  # SOPS
+  sops = {
+    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    secrets = {
+      nextcloud_url = {};
+      nextcloud_user = {};
+      nextcloud_pass = {};
+    };
+  };
+
+  # rclone
+  programs.rclone = {
+    enable = true;
+  };
+
+  # Nextcloud
+  systemd.user.services.nextcloud = {
+    Unit = {
+      Description = "Connect to Nextcloud using rclone";
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = pkgs.writeShellScript "rclone-setup" ''
+        mkdir -p $HOME/.config/rclone
+        
+        cat > $HOME/.config/rclone/rclone.conf <<EOF
+        [nextcloud]
+        type = webdav
+        url = $(cat ${config.sops.secrets.nextcloud_url.path})
+        vendor = nextcloud
+        user = $(cat ${config.sops.secrets.nextcloud_user.path})
+        pass = $(cat ${config.sops.secrets.nextcloud_pass.path})
+        EOF
+      
+        ${pkgs.rclone}/bin/rclone mount nextcloud: $HOME/Nextcloud --allow-non-empty --vfs-cache-mode writes
+      '';
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 
   # Zen Browser
   programs.zen-browser = {
@@ -52,7 +98,7 @@
       DisableAppUpdate = true;
       DisableTelemetry = true;
       Preferences = {
-        "widget.use-xdg-desktop-portal.file-picker" = { # Use KDE's file picker
+        "widget.use-xdg-desktop-portal.file-picker" = {
           Value = 1;
           Status = "locked";
         };
