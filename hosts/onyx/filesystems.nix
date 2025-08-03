0a3c28936fd6c5f7e4624ab31a9856482fa51301
Vladyslav Afanasiev <kutabe@protonmail.com>
2025-08-03 08:24:47 +0300

    add mountpoint, set default permissions

--- hosts/onyx/filesystems.nix
+++ hosts/onyx/filesystems.nix
@@ -1,7 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  fileSystems."/home".options = [ "compress=zstd:1" "noatime" "ssd" "discard=async" "space_cache=v2" ];
-  fileSystems."/mnt/games".options = [ "compress=zstd:-3" "noatime" "ssd" "discard=async" "space_cache=v2" ];
-  fileSystems."/mnt/steam-library".options = [ "compress=zstd:-3" "noatime" "ssd" "discard=async" "space_cache=v2" ];
+  fileSystems."/home".options = [ "compress=zstd:1" "noatime" "ssd" "discard=async" ];
+  fileSystems."/mnt/games".options = [ "compress=zstd:-3" "noatime" "ssd" "discard=async" ];
+  fileSystems."/mnt/steam-library".options = [ "compress=zstd:-3" "noatime" "ssd" "discard=async" ];
+  fileSystems."/mnt/backups".options = [ "compress=zstd:3" "noatime" "autodefrag" ];
+
+  systemd.tmpfiles.rules = [
+    "d /mnt/games 0755 cirno users -"
+    "d /mnt/steam-library 0755 cirno users -"
+    "d /mnt/backups 0755 cirno users -"
+  ];
 }
