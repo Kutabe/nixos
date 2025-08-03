8bc899dead9571d858aaad9be9682925e7e6a4de
Vladyslav Afanasiev <kutabe@protonmail.com>
2025-08-03 08:22:48 +0300

    change kernel to cachyos

--- hosts/onyx/boot.nix
+++ hosts/onyx/boot.nix
@@ -4,9 +4,17 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
-  boot.kernelPackages = pkgs.linuxPackages_lqx;
-  boot.kernelModules = [ "tcp_bbr" ];
+  boot.kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V3"; };
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+  boot.kernelModules = [ "tcp_bbr" "v4l2loopback" ];
+  
   boot.kernelParams = [ "amdgpu.dc=1" ];
+  
+  services.scx = {
+    enable = true;
+    scheduler = "scx_rustland";
+  };
   
   boot.kernel.sysctl = {
     "vm.max_map_count" = 1048576;
